7256402c8dcc91ad93a268fe83e9883df53142b2
Cameron Beatty <beatty2121@gmail.com>
2020-11-05 17:56:52 -0600

    initial commit

---
+++ WCFServiceExample/ExampleRun.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using SsrsService;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WCFServiceExample
+{
+    class ExampleRun
+    {
+        private const int MaxReceivedMessageSize = 20_971_529;
+        private readonly ReportExecutionServiceSoapClient _reportClient;
+
+        public ExampleRun(IConfiguration configuration)
+        {
+            _reportClient = new ReportExecutionServiceSoapClient(new BasicHttpBinding()
+            {
+                MaxReceivedMessageSize = MaxReceivedMessageSize,
+
+            }, new EndpointAddress(configuration["ReportExecutionServiceSoapUrl"]));
+            _reportClient.ClientCredentials.Windows.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Delegation;
+
+        }
+
+        public async Task TestAsync()
+        {
+            try
+            {
+                var trustedUserHeader = new TrustedUserHeader();
+                var reportPath = "replace with path to report in SSRS";
+                var report = await _reportClient.LoadReportAsync(trustedUserHeader, reportPath, null);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"An excpetion occurred: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ WCFServiceExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace WCFServiceExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = CreateHostBuilder(args).Build();
+            var example = serviceProvider.Services.GetRequiredService<ExampleRun>();
+            await example.TestAsync();
+
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+              
+                services.AddTransient<ExampleRun>();
+            });
+    }
+}
